c311d669c3a0dc6acd0814ee842241da967b0dc7
hojin0729 <155591134+hojin0729@users.noreply.github.com>
2024-01-11 08:26:18 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -3,7 +3,6 @@ class Solution {
 
         char[] arr = my_string.toCharArray();
         StringBuffer answer = new StringBuffer();
-        // arr = [h,e,l,l,o]
         for(char k : arr){
             for(int i=0; i<n;i++){
                 answer.append(k);
